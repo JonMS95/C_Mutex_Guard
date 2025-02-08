2d5164c2220b67a4a7ef32aee7b6dbd17097dc9f
JonMS95 <jon_portugalete@hotmail.com>
2025-02-08 18:51:30 +0100

    Required function prototypes. List will be expanded in a near future (arguably in a different file).

---
+++ src/TestDbgMutexes.h
@@ -0,0 +1,10 @@
+#ifndef TEST_DBG_THREADS_H
+#define TEST_DBG_THREADS_H
+
+/******* Shared function prototypes ******/
+
+void TestDbgMutexes(void);
+
+/*****************************************/
+
+#endif
