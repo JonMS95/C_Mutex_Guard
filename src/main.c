9db9ca8a5fad9142b65db6a3f9d65a14df29ae9a
JonMS95 <jon_portugalete@hotmail.com>
2025-02-23 17:57:16 +0100

    Added some code for time-performance to be measured and compared with POSIX ("common") mutexes.

--- src/main.c
+++ src/main.c
@@ -1,14 +1,54 @@
-// Compile with: gcc -g -Wall src/*.c -lpthread -o exe/main (use -D__DBG_MTX_FL__ too if required).
+// Compile with: gcc -g -Wall src/*.c -lpthread -o exe/main (use -D__DBG_MTX_FULL_BT__ too if required).
 
 /********** Include statements ***********/
 
 #include "TestDbgMutexes.h"
+#include <stdio.h>
+#include <time.h>
 
 /*****************************************/
 
+#define TEST_FN_TIME(function)   TestFunctionElapsedTime(function, #function)
+#define NUM_OF_TESTS    100
+
+double TestFunctionElapsedTime(void test_fn (void), char* test_fn_name)
+{
+    struct timespec start, end;
+    double time_taken;
+
+    // Record the start time
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    // Call the function you want to measure
+    test_fn();
+
+    // Record the end time
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    // Calculate the elapsed time in seconds (and nanoseconds)
+    time_taken = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+
+    printf("[ %s ]\tTime taken: %f seconds\n", test_fn_name, time_taken);
+
+    return time_taken;
+}
+
 int main()
 {
-    TestDbgMutexes();
+    // double time_records_common[NUM_OF_TESTS];
+    // double time_records_debug[NUM_OF_TESTS];
+    double avg_time_common  = 0;
+    double avg_time_debug   = 0;
 
+    for(int i = 0; i < NUM_OF_TESTS; i++)
+    {
+        // avg_time_common += (TEST_FN_TIME(TestCommonMutexes) / NUM_OF_TESTS);
+        avg_time_debug += (TEST_FN_TIME(TestDbgMutexes) / NUM_OF_TESTS);
+                avg_time_common += (TEST_FN_TIME(TestCommonMutexes) / NUM_OF_TESTS);
+    }
+
+    printf("Average time taken (common): %f\r\n", avg_time_common);
+    printf("Average time taken (debug):  %f\r\n", avg_time_debug);
+    
     return 0;
 }
