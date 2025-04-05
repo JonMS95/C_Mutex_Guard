82b8cac3e02c02574e1efd6b55aa94b29434a1e5
JonMS95 <jon_portugalete@hotmail.com>
2025-04-05 18:44:37 +0200

    Sligthly modified main file to organize it to fit new files.

--- test/src/main.c
+++ test/src/main.c
@@ -4,21 +4,38 @@
 #include "MutexGuard_api.h"
 #include "TestReturnValues.h"
 #include "TestErrorCodes.h"
+#include "TestDemos.h"
 
 /*****************************************/
 
-/*************** Run Tests ***************/
+
+/*********** Define statements ***********/
+
+#define TEST_UNIT_TESTS_HEADER  "\r\n**********\r\nUNIT TESTS\r\n**********\r\n"        
+#define TEST_USAGE_DEMOS_HEADER "\r\n***********\r\nUSAGE DEMOS\r\n***********\r\n"
+
+/*****************************************/
+
+/********** Function definitions *********/
 
 int main()
 {
+    // Unit tests
     if(CU_initialize_registry() != CUE_SUCCESS)
         return CU_get_error();
     
     CreateReturnValueTestsSuite();
     CreateErrorCodeTestsSuite();
     
+    printf("%s", TEST_UNIT_TESTS_HEADER);
     CU_basic_run_tests();
     CU_cleanup_registry();
 
+    // Usage demos
+    printf("%s", TEST_USAGE_DEMOS_HEADER);
+    TestDemo();
+
     return 0;
 }
+
+/*****************************************/
