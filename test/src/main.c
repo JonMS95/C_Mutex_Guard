64df88d2dbfdeae91167efccaf5d4ba443983be8
JonMS95 <jon_portugalete@hotmail.com>
2025-05-10 17:35:39 +0200

    Using severity log instead of printf.

--- test/src/main.c
+++ test/src/main.c
@@ -5,6 +5,7 @@
 #include "TestReturnValues.h"
 #include "TestErrorCodes.h"
 #include "TestDemos.h"
+#include "SeverityLog_api.h"
 
 /*****************************************/
 
@@ -20,6 +21,9 @@
 
 int main()
 {
+    // Init severity log
+    SeverityLogInit(1000, SVRTY_LOG_MASK_ALL, false, false, true);
+
     // Unit tests
     if(CU_initialize_registry() != CUE_SUCCESS)
         return CU_get_error();
@@ -27,12 +31,12 @@ int main()
     CreateReturnValueTestsSuite();
     CreateErrorCodeTestsSuite();
     
-    printf("%s", TEST_UNIT_TESTS_HEADER);
+    SVRTY_LOG_INF(TEST_UNIT_TESTS_HEADER);
     CU_basic_run_tests();
     CU_cleanup_registry();
 
     // Usage demos
-    printf("%s", TEST_USAGE_DEMOS_HEADER);
+    SVRTY_LOG_INF(TEST_USAGE_DEMOS_HEADER);
     TestDemo();
 
     return 0;
